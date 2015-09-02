ce71513c785bed28b5bb98d5a4a2de5d5719edd2
mikey1977 <mmi@umich.edu>
2015-09-01 18:03:35 -1000

    First two tests passed

--- multiples.js
+++ multiples.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * sumOfAMultiple() sums of the multiples of 3 and 5 for any number passed in
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
@@ -6,7 +8,12 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
-  // do your work here
 
+  // do your work here
+  for (var i = 0; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
   return sum;
 };
